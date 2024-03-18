aca8eb2663333391413e942325f62019c8b36fda
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-03-18 16:11:23 -0300

    refactor: Create order split into two more functions

--- backend/src/models/OrdersModel.ts
+++ backend/src/models/OrdersModel.ts
@@ -5,38 +5,13 @@ export default class OrdersModel {
 	private ordersModel = new PrismaClient()
 
 	async create({ discount, total, userId, address, productsList }: IOrderRequest): Promise<IOrder> {
-		const findUser = await this.ordersModel.user.findUnique({ where: { id: userId }})
-
-		if (!findUser) {
-			throw new Error('User not found.')
-		}
-
-		if (!findUser.address) {
-			throw new Error('User address not found.')
-		}
-
-		if (discount < 0 || total < 0) {
-			throw new Error('Invalid discount or total value.')
-		}
-
-		if (productsList.length < 1) {
-			throw new Error('Invalid products list.')
-		}
-
-		await Promise.all(
-			productsList.map(async (product) => {
-				const findProduct = await this.ordersModel.product.findUnique({ where: { id: product.productId }})
-				if (!findProduct) {
-					throw new Error('Invalid product in products list.')
-				}
-			})
-		)
+		await this.findProducts(productsList)
           
 		const newOrder = await this.ordersModel.order.create({
 			data: {
 				discount,
 				total,
-				userId: findUser.id,
+				userId,
 				OrderProduct: {
 					create: await Promise.all(productsList.map(async ({ productId, quantity, color, size }) => {
 						const productVariant = await this.ordersModel.productVariant.findUnique({
@@ -71,6 +46,8 @@ export default class OrdersModel {
 			throw new Error('Unable to create new order.')
 		}
 
+		await this.updateStock(productsList)
+		
 		return {
 			id: newOrder.id,
 			discount,
@@ -84,5 +61,53 @@ export default class OrdersModel {
 				quantity
 			}))
 		}
+	}
+
+	async findProducts(productsList: IOrderRequest['productsList']) {
+		await Promise.all(
+			productsList.map(async (product) => {
+				const findProduct = await this.ordersModel.product.findUnique({ where: { id: product.productId }})
+				if (!findProduct) {
+					throw new Error('Invalid product in products list.')
+				}
+			})
+		)
+	}
+
+	async updateStock(productsList: IOrderRequest['productsList']) {
+		await Promise.all(
+			productsList.map(async ({ productId, quantity, color, size }) => {
+				const productVariant = await this.ordersModel.productVariant.findUnique({
+					where: {
+						color_size_productId: {
+							color, size, productId
+						}
+					}
+				})
+		
+				if (!productVariant) {
+					throw new Error('Product variant not found.')
+				}
+		
+				const newProductQuantity = productVariant.quantity - quantity
+		
+				if (newProductQuantity < 0) {
+					throw new Error('Not enough stock for this product variant.')
+				}
+			
+				await this.ordersModel.productVariant.update({
+					where: {
+						color_size_productId: {
+							color: productVariant.color,
+							size: productVariant.size,
+							productId: productVariant.productId
+						}
+					},
+					data: {
+						quantity: newProductQuantity
+					}
+				})
+			})
+		)
 	}
 }
