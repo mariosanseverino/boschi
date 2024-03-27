e6c7ec5fc7ca3fbc45ff55ff202700983bc9bf96
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-03-27 15:02:46 -0300

    feat: Remove product from cart added

--- frontend/src/app/components/ShopCart.tsx
+++ frontend/src/app/components/ShopCart.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { useShopCartContext } from '../contexts/ShopCartContext'
+import { Product } from '../interfaces/products/Products'
 
 export default function ShopCart() {
-	const { cartProducts } = useShopCartContext()
+	const { cartProducts, setCartProducts } = useShopCartContext()
     
+	function removeFromCart(removedProduct: Product) {
+		const currentCart = [...cartProducts]
+		const newCart = currentCart.filter((product) => product.id !== removedProduct.id)
+		setCartProducts(newCart)
+	}
+
 	return (
 		<div>
 			<p>Shopping Cart</p>
 			{ cartProducts.length < 1
 				? <p>Empty</p>
-				: cartProducts.map((cartProduct, index) => (<p key={ index }>{ cartProduct.name }</p>))
+				: cartProducts.map((cartProduct, index) => (
+					<div key={ index }>
+						<p>{ cartProduct.name }</p>
+						<button
+							onClick={ () => removeFromCart(cartProduct) }
+						>
+							remove
+						</button>
+					</div>
+				))
 			}
 		</div>
 	)
